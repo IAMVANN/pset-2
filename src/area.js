179818ffeb972f6ccaa9ef32065b9e60b0051246
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-10-22 10:29:16 -0400

    area finsihed

--- src/area.js
+++ src/area.js
@@ -1,6 +1,7 @@
 const scanner=require("readline-sync");
 const width=scanner.question("\nWidth: ");
 const length=scanner.question("Length: ");
-const areaInMili= Math.round(width * length * 25.4 * 100)/100;
-
+let rounder= Math.round(width * length * 25.4 * 100)/100;
+//let rounder= (width*length*25.4).toFixed(2);
+let areaInMili=rounder.toLocaleString('en');
 console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has an area of " + areaInMili + " square milimeter(s).");
