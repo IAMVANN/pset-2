bf4cf41932d415cbcbe06ea99afddd3295ddc1b9
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-10-22 10:49:14 -0400

    finished perimeter

--- src/area.js
+++ src/area.js
@@ -1,7 +1,7 @@
 const scanner=require("readline-sync");
 const width=scanner.question("\nWidth: ");
 const length=scanner.question("Length: ");
-const areaInMili=width*length*25.4;
+const areaInMili=width*25.4*length*25.4;
 const rounder= Math.round(areaInMili * 100)/100;
 //let rounder= (width*length*25.4).toFixed(2);
 const finalarea=rounder.toLocaleString('en');
---
+++ src/perimeter.js
@@ -0,0 +1,8 @@
+const scanner=require("readline-sync");
+const width=scanner.question("\nWidth: ");
+const length=scanner.question("Length: ");
+const perimeterInCenti=(2*width*2.54+2*length*2.54);
+const rounder=Math.round(perimeterInCenti*100)/100;
+const finalperimeter=rounder.toLocaleString('en');
+
+console.log("/nA(n) " + width + " -by-" + length + "-inch sheet of paper has aperimeter of " + finalperimeter + " cenimeter(s).")
